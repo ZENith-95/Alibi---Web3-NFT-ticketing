2f1cce9e1d00626bae30ab40584d5585ab5b18fa
EurikaOrmanel <wworgba@gmail.com>
2025-04-29 00:11:08 +0000

    using new navbar

--- src/alibi_frontend/src/components/layouts/MainLayout.tsx
+++ src/alibi_frontend/src/components/layouts/MainLayout.tsx
@@ -1,16 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Topbar from "../ui/Topbar";
+import { SiteHeader } from "../SiteHeader";
 // import { NavBar } from "./NavBar";
 
 const MainLayout = () => {
     return (
         <div className="min-h-screen bg-gradient-to-br from-[#0F0F1A] to-[#1A1A2C] text-[#E0E0FF]">
+            <SiteHeader />
 
-            <div className="flex-1">
-                <Topbar />
+            <main>
+
                 <Outlet />
-            </div>
-        </div>
+            </main>
+
+        </div >
     );
 };
 export default MainLayout
--- src/alibi_frontend/src/pages/HomePage.tsx
+++ src/alibi_frontend/src/pages/HomePage.tsx
@@ -6,7 +6,6 @@ import { SiteHeader } from "../components/SiteHeader"
 export default function HomePage() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-[#0F0F1A] to-[#1A1A2C] text-[#E0E0FF]">
-      <SiteHeader />
       <main>
         <HeroSection />
         <EventDiscovery />
